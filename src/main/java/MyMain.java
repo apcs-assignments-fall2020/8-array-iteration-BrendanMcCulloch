64ecadedfdf9119d597d46ceb579a4e52e44595b
BrendanMcCulloch <22bmcculloch@athenian.org>
2020-10-18 03:20:07 +0000

    retry

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,34 +1,43 @@
-import java.util.Scanner;
 public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        for (int i = 0; i < arr.length/2; i++){
-            int temp = arr[i];
-            arr[i] = arr[arr.length - i - 1];
-            arr[arr.length - i - 1] = temp;
+        int [] jojo = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            jojo[i] = arr[arr.length - 1 - i];
+
         }
-        return arr;
+        return jojo;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        for (int i = 0; i < arr.length; i++){
-            for (int j = i+1; j < arr.length; j++){
-                if (arr[j] > arr[i]){
-                   int temp = arr[i];
-                   arr[i] = arr[j];
-                   arr[j] = temp;
-                }
+        int y, r;
+        if (arr[0] > arr[1]){
+            y = arr[0];
+            r = arr[1];
+
+        }
+        else {
+            y = arr[1];
+            r = arr[0];
+        }
+        for (int i = 2; i < arr.length; i++){
+            if ((arr[i] <= y) && arr[i] > r){
+                r = arr[i];
+            }
+            if (arr[i] > y){
+                r = y;
+                y = arr[i];
             }
         }
-        return arr[arr.length - 2];
+        return r;
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
         double[] factor = new double[arr.length-1];
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length - 1; i++){
             factor[i] = Double.valueOf(arr[i+1])/Double.valueOf(arr[i]);
         }
         int count = 0;
@@ -48,32 +57,8 @@ public class MyMain {
 
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("number of values in array");
-        int n_1 = scan.nextInt();
-        int arr_1[] =  new int[n_1];
-        System.out.println("enter all the elements: ");
-        for (int i = 0; i < n_1; i++){
-            arr_1[i] = scan.nextInt();
-        }
-        System.out.println(reverse(arr_1));
-        System.out.print("number of values in array");
-        int n_2 = scan.nextInt();
-        int arr_2[] =  new int[n_2];
-        System.out.println("enter all the elements: ");
-        for (int i = 0; i < n_2; i++){
-            arr_2[i] = scan.nextInt();
-        }
-        System.out.println(secondLargest(arr_2));
-        System.out.print("number of values in array");
-        int n_3 = scan.nextInt();
-        int arr_3[] =  new int[n_3];
-        System.out.println("enter all the elements: ");
-        for (int i = 0; i < n_3; i++){
-            arr_2[i] = scan.nextInt();
-        }
-        System.out.println(isGeometric(arr_3));
-        
-        scan.close();
+        System.out.println(reverse(null));
+        System.out.println(secondLargest(null));
+        System.out.println(isGeometric(null));
     } 
 }
